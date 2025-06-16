d3a08df9a71f4918aa903f9e1a4083b16ee787f5
Gurur Kısaoğlu <kisaoglugurur@gmail.com>
2025-06-16 05:11:09 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Gurur on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
